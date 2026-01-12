5d8e59c57879852fd4ed639cfcfe2713f0929232
klfministries <klfministries7@gmail.com>
2026-01-11 23:59:57 -0500

    Add clickable Instagram icon to contact section

--- app/page.jsx
+++ app/page.jsx
@@ -186,7 +186,26 @@ export default function AuthorWebsite() {
             </a>
           </p>
           <p>📞 +1 876 870 0508</p>
-          <p>📸 @kiwayne27</p>
+          <p className="flex justify-center items-center gap-2">
+  <a
+    href="https://instagram.com/kiwayne27"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="text-pink-600 hover:text-pink-700 flex items-center gap-2"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+    >
+      <path d="M7.75 2h8.5A5.75 5.75 0 0 1 22 7.75v8.5A5.75 5.75 0 0 1 16.25 22h-8.5A5.75 5.75 0 0 1 2 16.25v-8.5A5.75 5.75 0 0 1 7.75 2Zm0 1.5A4.25 4.25 0 0 0 3.5 7.75v8.5A4.25 4.25 0 0 0 7.75 20.5h8.5a4.25 4.25 0 0 0 4.25-4.25v-8.5A4.25 4.25 0 0 0 16.25 3.5h-8.5Zm8.9 2.25a.85.85 0 1 1 0 1.7.85.85 0 0 1 0-1.7ZM12 7a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 1.5a3.5 3.5 0 1 0 0 7 3.5 3.5 0 0 0 0-7Z" />
+    </svg>
+    <span>@kiwayne27</span>
+  </a>
+</p>
+
         </section>
       )}
 
@@ -202,4 +221,5 @@ export default function AuthorWebsite() {
     </div>
   );
 }
+
 
