7f22cd64ba53d44096e0d7e42c96a413438c5487
klfministries <klfministries7@gmail.com>
2026-01-13 11:52:27 -0500

    Add GA4 tracking for PayPal and WhatsApp clicks

--- app/components/Analytics.jsx
+++ app/components/Analytics.jsx
@@ -6,8 +6,8 @@ export default function Analytics() {
   return (
     <>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
         strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-RY05JQHSR1"
       />
       <Script id="ga4" strategy="afterInteractive">
         {`
@@ -15,6 +15,15 @@ export default function Analytics() {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
           gtag('config', 'G-RY05JQHSR1');
+
+          document.addEventListener('click', function(e) {
+            const event = e.target.getAttribute('data-event');
+            if (event) {
+              gtag('event', event, {
+                method: 'engagement'
+              });
+            }
+          });
         `}
       </Script>
     </>
