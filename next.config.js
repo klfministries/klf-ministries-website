af4a6b99700cc5a6f0e1adc22f92975c42f909c4
klfministries <klfministries7@gmail.com>
2026-01-13 06:47:31 -0500

    Enable internationalization (i18n) support

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+  },
 };
 
 module.exports = nextConfig;
-
