55d432bec233465f01469292e3d3b359e8af65be
klfministries <klfministries7@gmail.com>
2026-01-13 10:52:15 -0500

    Add multilingual sitemap with hreflang support

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,8 +1,32 @@
 export default function sitemap() {
-  return [
-    {
-      url: "https://klfministries.org",
-      lastModified: new Date(),
-    },
+  const baseUrl = "https://klfministries.org";
+
+  const routes = [
+    "",
+    "/about",
+    "/books",
+    "/videos",
+    "/contact",
   ];
+
+  const languages = ["en", "es"];
+
+  const pages = [];
+
+  languages.forEach((lang) => {
+    routes.forEach((route) => {
+      pages.push({
+        url: `${baseUrl}/${lang}${route}`,
+        lastModified: new Date(),
+        alternates: {
+          languages: {
+            en: `${baseUrl}/en${route}`,
+            es: `${baseUrl}/es${route}`,
+          },
+        },
+      });
+    });
+  });
+
+  return pages;
 }
